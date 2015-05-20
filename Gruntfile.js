e9c8dc3bf53c0f600fa0d7eb90a97dfaa7378be7
tsmuse <tony@notjustprettypictures.com>
2015-05-20 14:43:47 -0700

    all configured and ready to start building

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,51 @@ module.exports = function(grunt){
 		pkg: grunt.file.readJSON('package.json'),
 
 		//tasks
+		watch: {
+			compass: {
+				files: ['sass/*.{scss,sass}'],
+				tasks: ['compass:dev', 'jekyll:dev']
+			},
+			jekyll: {
+				files: ['_includes/*.html', '_layouts/*.html', '*.html', '_posts/*.{html,md}'],
+				tasks: ['jekyll:dev']
+			}
+		},
+		compass: {
+			dev: {
+				options: {
+					config: 'config.rb',
+					environment: 'development',
+					outputStyle: 'nested'
+				}
+			},
+			prod: {
+				options: {
+					config: 'config.rb',
+					environment: 'production'
+				}
+			}
+		},
+		jekyll: {
+			dev: {
+				options:{
+					config: '_config.dev.yml'
+				}
+
+			},
+			build: {
+				
+			}
+		}
 	});
 
-	//Load the plugin that provides the "uglify" task
-	grunt.loadNPMTasks('');
+	//Load the plugins
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-jekyll');
 
 	//Default task(s)
-	grunt.registerTask('default',['']);
+	grunt.registerTask("default", ['watch']);
 
 };
